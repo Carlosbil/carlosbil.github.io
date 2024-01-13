3ffc5dd7a88a49340e64548ac18dc1c3169991d2
Carlosbil <carlosbilbao2@gmail.com>
2024-01-13 12:11:55 +0100

    both styles

---
+++ scripts.js
@@ -0,0 +1,6 @@
+export function toggleStyle() {
+    var style1 = document.getElementById('style1');
+    var style2 = document.getElementById('style2');
+    style1.disabled = !style1.disabled;
+    style2.disabled = !style2.disabled;
+}

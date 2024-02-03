c9471627ba4d2489c2f2d9b7bf4b07a7161518be
Carlosbil <carlosbilbao2@gmail.com>
2024-02-03 15:39:12 +0100

    some advances

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 function toggleStyle() {
-    var style1 = document.getElementById('style1');
-    var style2 = document.getElementById('style2');
+    var style1 = document.getElementById('dark');
+    var style2 = document.getElementById('light');
     style1.disabled = !style1.disabled;
     style2.disabled = !style2.disabled;
 }
